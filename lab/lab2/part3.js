d448e93ebd6553e52fcd5c13d896c7ca10229915
Jeff Frankl <jfrankl@azavea.com>
2017-01-27 14:24:39 -0500

    Fix formatting

--- lab/lab2/part3.js
+++ lab/lab2/part3.js
@@ -14,11 +14,13 @@ Let's revisit the bike share data from Week 2 Lab 1, Part 4. Remember, each arra
 
 We're especially interested in number 4: number of bike share docks at the station.
 
-Using underscore functions, generate a list of all bike share docks that are greater than 20. One way to do this is by using _.filter, but you can try other solutions as well. Set your answer to variable "largeStationList".
+Using underscore functions, generate a list of all bike share docks that are greater than 20. One way to do 
+this is by using _.filter, but you can try other solutions as well. Set your answer to variable "largeStationList".
 
 ## Task 2
 
-Let's say we only care about the final count of bike share locations with more than 20 docks. Calculate the value by using _.countBy and set your answer to variable "largeStationCount".
+Let's say we only care about the final count of bike share locations with more than 20 docks. Calculate the value
+by using _.countBy and set your answer to variable "largeStationCount".
 ===================== */
 
 var data = bikeArrayClean;
